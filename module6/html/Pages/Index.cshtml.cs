fe157893c9c4f7697a2db5eb05d22358de0e4c07
Kenzie Gibson <gibsonkenzie76@gmail.com>
2026-01-04 19:47:24 +0000

    Form now functional, frontend data submitted to backend and processed

--- module6/html/Pages/Index.cshtml.cs
+++ module6/html/Pages/Index.cshtml.cs
@@ -4,9 +4,16 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace html.Pages;
 
 public class IndexModel : PageModel
-{
-    public void OnGet()
-    {
+{   
+    [BindProperty]
+    public string Username{get; set;} = ""; // stores username from frontend form, "" for if user doesnt submit anything, avoids error down the line (same as required in cshtml -  i prefer required)
 
+    [BindProperty]
+    public bool Newsletter{get; set;} // stores if user wants to subscribe to a newsletter
+
+    public bool Submitted{get; set;} // stores if the form has been submitted?
+    public void OnPost() // conventional naming method
+    {
+        Submitted = true;
     }
 }
